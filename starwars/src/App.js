3be166e129a836840ae6f2a839e74a71a59d932f
ericbanker12 <ericbanker12@gmail.com>
2019-09-13 10:57:03 -0500

    fetch people data

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [peopleState, setPeopleState] = useState({})
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get(`https://swapi.co/api/people`)
+      .then(response => {
+        // console.log(response)
+        setPeopleState(response.data)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+
+    // Example response.data
+    // {
+    //   count: 87
+    //   next: "https://swapi.co/api/people/?page=2"
+    //   previous: null
+    //   results: [{
+    //     birth_year: "19BBY"
+    //     created: "2014-12-09T13:50:51.644000Z"
+    //     edited: "2014-12-20T21:17:56.891000Z"
+    //     eye_color: "blue"
+    //     films: [
+    //       "https://swapi.co/api/films/2/",
+    //       "https://swapi.co/api/films/6/",
+    //       "https://swapi.co/api/films/3/",
+    //       "https://swapi.co/api/films/1/",
+    //       "https://swapi.co/api/films/7/"
+    //     ]
+    //     gender: "male"
+    //     hair_color: "blond"
+    //     height: "172"
+    //     homeworld: "https://swapi.co/api/planets/1/"
+    //     mass: "77"
+    //     name: "Luke Skywalker"
+    //     skin_color: "fair"
+    //     species: [
+    //       "https://swapi.co/api/species/1/"
+    //     ]
+    //     starships: [
+    //       "https://swapi.co/api/starships/12/",
+    //       "https://swapi.co/api/starships/22/"
+    //     ]
+    //     url: "https://swapi.co/api/people/1/"
+    //     vehicles: [
+    //       "https://swapi.co/api/vehicles/14/",
+    //       "https://swapi.co/api/vehicles/30/"
+    //     ]
+    //   }]
+    // }
+
+  },[])
 
   return (
     <div className="App">
